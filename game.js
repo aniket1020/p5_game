bc8103052fc4ba3082fb4d2c35d4e4eb765a8980
Aniket Inamdar <50190428+aniket1020@users.noreply.github.com>
2019-11-09 21:49:53 +0530

    Add files via upload

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 
-var scr=0;
+var scr=0,tmp1,tmp2,tmp=0;
 var frX,frY;
 function basket(x,y,a,b){
 let c =  color(255,204,0)
@@ -18,22 +18,75 @@ rect(x+(a*3),y-b,a,b);
 }
 class Fruit{
   spawn(){
-    fval=1;
-    frX=floor(random((bw-350),(bw+350)));
+    fval=true;
+    tmp1=bw-250;
+    if(tmp1<0)
+    tmp1=0;
+    tmp2=bw+250;
+    if(tmp2>1250)
+    tmp2=1250;
+    frX=floor(random((tmp1),(tmp2)));
     frY=100;
     rect(frX,frY,15,15);
+    rect(frX-15,frY,15,15);
+    rect(frX+15,frY,15,15);
+    rect(frX,frY+15,15,15);
+    rect(frX,frY-15,15,15);
   }
   drive(){
-    frY+=5;
+    frY+=spd;
     rect(frX,frY,15,15);
+    rect(frX-15,frY,15,15);
+    rect(frX+15,frY,15,15);
+    rect(frX,frY+15,15,15);
+    rect(frX,frY-15,15,15);
   }
   check(){
-    if(frY==(bh-5) && (frX>=(bw-45) && frX<=(bw+45))){
-      fval=0;
+    if(frY>=(bh-5) && (frX>=(bw-45) && frX<=(bw+45))){
+      fval=false;
       scr=scr+1;
     }
-    else if(frY==(bh-5)){
-      fval=0;
+    else if(frY>=(bh-5)){
+      fval=false;
+    }
+    }
+}
+class Poison{
+  spawn(){
+    fval=true;
+    tmp1=bw-250;
+    if(tmp1<0)
+    tmp1=0;
+    tmp2=bw+250;
+    if(tmp2>1250)
+    tmp2=1250;
+    frX=floor(random((tmp1),(tmp2)));
+    frY=100;
+    fill(255,0,0);
+    rect(frX,frY,15,15);
+    rect(frX+15,frY+15,15,15);
+    rect(frX-15,frY+15,15,15);
+    rect(frX-15,frY-15,15,15);
+    rect(frX-15,frY+15,15,15);
+  }
+  drive(){
+    frY+=spd;
+    fill(255,0,0);
+    rect(frX,frY,15,15);
+    rect(frX+15,frY+15,15,15);
+    rect(frX-15,frY+15,15,15);
+    rect(frX-15,frY-15,15,15);
+    rect(frX+15,frY-15,15,15);
+  }
+  check(){
+    if(frY>=(bh-5) && (frX>=(bw-45) && frX<=(bw+45))){
+      fval=false;
+      if(scr!=0){
+      scr=scr-1;
+      }
+    }
+    else if(frY>=(bh-5)){
+      fval=false;
     }
     }
 }
@@ -41,7 +94,11 @@ class Fruit{
 function score(){
   textStyle(BOLD);
   textSize(25);
-  fill('#0f0');
+  fill('purple');
   text('SCORE : ',600,50);
   text(scr,725,50);
+  if(scr%5==0 && tmp!=scr){
+    tmp=scr;
+    spd+=1;
+  }
   }
--- sketch.js
+++ sketch.js
@@ -1,27 +1,40 @@
 var bw=650,bh=500,vx=1;
 var canw=1350,canh=550;
-var fruit,fval=0;
+var fruit,fval=false,som,spd=5;
 function setup(){
 createCanvas(canw,canh);
 fruit = new Fruit();
+poison = new Poison();
 }
 function draw(){
   frameRate(20);
   background(51);
   basket(bw,bh,15,15);
-  if(fval==0){
+  if(!fval){
+  if(floor(random(2))){
   fruit.spawn();
+  som=true;
   }
+  else {
+     poison.spawn();
+     som=false;
+  }}
   if(vx==1){
   if(bw<(canw-60))
-  bw=bw+5;
+  bw=bw+spd;
   }
   else if(vx==-1){
   if(bw>45)
-  bw=bw-5;
+  bw=bw-spd;
   }
+  if(som){
   fruit.drive();
   fruit.check();
+  }
+  else {
+    poison.drive();
+    poison.check();
+  }
   score();
 }
 function keyPressed(){
